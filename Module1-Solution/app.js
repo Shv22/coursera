34cf569b3c05d1dd95b71994cd3cfd6a5edc1110
Shubham Vashishtha <shubhamvashistha22@gmail.com>
2018-08-19 17:43:16 +0530

    Next

---
+++ Module1-Solution/app.js
@@ -0,0 +1,29 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LCController);
+
+LCController.$inject = ['$scope'];
+function LCController($scope) {
+  $scope.items = "";
+  $scope.message = "";
+  $scope.checkItem = function(){
+    var numberOfItems = calculateItems($scope.items)
+    $scope.message = numberOfItems;
+  }
+  function calculateItems(string) {
+    var items = string.split(",")
+    if(items[0] == ""){              //checking for empty string
+      return "Please enter data first" ;
+    }
+    else if (items.length < 4) {
+      return "Enjoy!" ;
+    }
+    else {
+      return "Too much!" ;
+    }
+  }
+}
+
+})();
